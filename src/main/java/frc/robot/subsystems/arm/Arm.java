08df5735f7c4c53ec5c5b174da55eab46691490f
ColinH0 <119466406+ColinH0@users.noreply.github.com>
2023-08-13 22:26:28 -0400

    Fixed up and down functions. Removed hardware

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -4,21 +4,12 @@ import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 import frc.robot.Constants;
 
 public class Arm extends SubsystemBase {
   private final ArmIO io;
   private final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
   private final ArmVisualizer viz = new ArmVisualizer();
-
-  private final CANSparkMax leftArmMotor = new CANSparkMax(0 /* Placeholder ID */, MotorType.kBrushless);
-  private final CANSparkMax rightArmMotor = new CANSparkMax(0 /* Placeholder ID */, MotorType.kBrushless);
-
-  private final AbsoluteEncoder encoder;
 
   private final PIDController pidController;
 
@@ -27,29 +18,24 @@ public class Arm extends SubsystemBase {
   /** Creates a new Arm. */
   public Arm(ArmIO io) {
     this.io = io;
-    encoder = leftArmMotor.getAbsoluteEncoder(Type.kDutyCycle);
-
-    rightArmMotor.follow(leftArmMotor);
 
     pidController = new PIDController(Constants.ArmConstants.kP, Constants.ArmConstants.kI, Constants.ArmConstants.kD);
 
   }
 
   public void moveUp() {
-    leftArmMotor.set(1);
+    this.setpoint += 10;
   }
 
   public void moveDown() {
-    leftArmMotor.set(-1);
-  }
-
-  public void moveToSetpoint() {
-    leftArmMotor.set(pidController.calculate(encoder.getPosition(), setpoint));
+    this.setpoint -= 10;
   }
 
   @Override
   public void periodic() {
     // Update and log inputs
+    io.set(pidController.calculate(inputs.positionRev, setpoint));
+
     io.updateInputs(inputs);
     Logger.getInstance().processInputs("Arm", inputs);
 
