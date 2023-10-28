d72cd6c92afc03c6a041ef097011109b012c8826
Ana Badea <58919489+anatheiguana@users.noreply.github.com>
2023-10-27 22:11:37 -0400

    Swerve Changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -91,15 +91,15 @@ public final class Constants {
     public static final double kChassisAngularOffset = 0;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 2;
-    public static final int kRearLeftDrivingCanId = 6;
-    public static final int kFrontRightDrivingCanId = 4;
-    public static final int kRearRightDrivingCanId = 8;
+    public static final int kFrontLeftDrivingCanId = 4;
+    public static final int kRearLeftDrivingCanId = 8;
+    public static final int kFrontRightDrivingCanId = 2;
+    public static final int kRearRightDrivingCanId = 6;
 
-    public static final int kFrontLeftTurningCanId = 3;
-    public static final int kRearLeftTurningCanId = 7;
-    public static final int kFrontRightTurningCanId = 5;
-    public static final int kRearRightTurningCanId = 9;
+    public static final int kFrontLeftTurningCanId = 5;
+    public static final int kRearLeftTurningCanId = 9;
+    public static final int kFrontRightTurningCanId = 3;
+    public static final int kRearRightTurningCanId = 7;
 
     public static final boolean kGyroReversed = false;
 
@@ -155,7 +155,7 @@ public final class Constants {
 }
 
   public static final class ArmConstants{
-    public static final double kP = 1;
+    public static final double kP = 3;
     public static final double kI = 0;
     public static final double kD = 0;
 
